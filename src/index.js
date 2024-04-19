8942cb57ec23babf46fa7a92e35c9d29572d036c
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-04-19 09:44:47 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import Header from './Pages/Header/Header';
+import Sidebar from './Pages/Sidebar/Sidebar';
+import Welcome from './Pages/Welcome/Welcome';
+import './global.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div className='container'>
+            <Header />
+            <Sidebar />
+            <Welcome />
+    </div>
 );
+
